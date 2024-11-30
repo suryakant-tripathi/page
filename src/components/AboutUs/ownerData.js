4c4d0a46064e3a000bebdb6a71f55b85e05955df
suryakant-tripathi <heyitsabhinav04@gmail.com>
2024-11-30 21:41:10 +0530

    Add files via upload

---
+++ src/components/AboutUs/ownerData.js
@@ -0,0 +1,30 @@
+const ownerData = {
+  
+  yearsOfExperience: "35+",
+  subheading: "Years of Experience",
+  yearDescription: "in Ayodhya & Ranchi including Sangeetmay Pravachan Bhagwat & Ramcharit Manas at Bhagwat Vachak",
+
+  happyClients: "11K+",
+  clientsubheading: "Pujas Performed",
+  clientDescription: "in different states of India",
+
+  knowledgeCount: "1k+",
+  knowledgeHeading: "Pujas & Havan knowledge",
+  knowledgeDescription: "like Karmkand, Jyotish, Yagya, Ramcharit Manas Path, Sundarkand, Tatrik, etc.",
+
+  language: "3+",
+  languageSubheading: "Languages Known",
+  languageDescription: "Hindi, Vedic Sanskrit and English, etc",
+};
+
+export default ownerData;
+
+/**
+ * Vedic Sanskrit and English language
+ * sangeetmay pravachan Bhagwat & Ramcharit Manas at Bhagwat Vachak
+ * karmkand
+ * jyotish
+ *
+ *
+ *
+ * */
